ecd05409786e66d152dc51380415f239f0849f7b
JunHao Tan <244296129@qq.com>
2019-05-08 17:13:03 +0800

    Update Package.swift
    argument 'targets' must precede argument 'dependencies'

--- Package.swift
+++ Package.swift
@@ -3,16 +3,16 @@ import PackageDescription
 
 let package = Package(
     name: "Hello",
+    targets: [
+        .target(name: "App", dependencies: ["Leaf", "Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+    ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
         // 🍃 An expressive, performant, and extensible templating language built for Swift.
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
-    ],
-    targets: [
-        .target(name: "App", dependencies: ["Leaf", "Vapor"]),
-        .target(name: "Run", dependencies: ["App"]),
     ]
 )
 
